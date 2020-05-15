cedde00813b2bb7403b9897805b95411d82a9030
Daniel Persson <mailto.woden@gmail.com>
2020-05-15 21:31:21 +0200

    Another one bites the dust.

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # kub-autoscaling-example
-An example on how to setup auto-scaling in a kubernetics cluster.
+
+An example on how to setup auto-scaling in a kubernetes cluster.
+
+Parts of this example is inspired by this repository which shows the functionality to autoscale with a rabbitMQ queue.
+```
+https://github.com/onfido/k8s-rabbit-pod-autoscaler
+```
+
+First we need to download docker tools, minikube, kubectl, helm and put them in our path.
+
+```
+https://github.com/docker/toolbox/releases
+https://kubernetes.io/docs/tasks/tools/install-minikube/
+https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-windows
+https://github.com/helm/helm/releases
+```
+
+Putting your installation in the path on windows and on my system
+they are in the tools directory so I will need to run the command
+below.
+```
+set PATH=%PATH%;c:\tools
+```
+
+First we need to start minikube so we have the system created and running.
+```
+minikube start
+```
+
+In order to install the metrics server we use to follow how much resourses our pods are using we use the command below.
+```
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+```
+
+Alternative with minikube there is a addon called metrics server so we don't need any extra setup.
+```
+minikube addons list
+minikube addons enable metrics-server
+```
+
+Next up we install our application, here we have a very simple application that will generate some work using a square root function.
+```
+kubectl apply -f https://k8s.io/examples/application/php-apache.yaml
+```
+
+Next up we setup an auto-scaler manually with a minimum of 1 pod and a maximum of 10 pods changing when a pod have more than 50% CPU utilization.
+```
+kubectl autoscale deployment php-apache --cpu-percent=50 --min=1 --max=10
+```
+
+Using kubectl we can look at the scaling functionallity.
+```
+kubectl get hpa
+```
+
+Next up we will create some load and see the scaler working.
+```
+kubectl run -it --rm load-generator --image=busybox /bin/sh
+
+Hit enter for command prompt
+
+while true; do wget -q -O- http://php-apache; done
+```
+
+If you want to read the original guide from kubernetes you have that guide below.
+```
+https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
+```
+
+Last example we will look into is how to scale your pods depending on a rabbitMQ queue. Creating and starting the administration GUI we can do that following the commands below.
+```
+kubectl create namespace rabbit
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install mu-rabbit bitnami/rabbitmq --namespace rabbit
+kubectl port-forward --namespace rabbit svc/mu-rabbit-rabbitmq 15672:15672
+```
+
+Next up we need the password to login. We can retrieve it with the command below.
+```
+bla bla bla
+```
+
+In order to build and deploy the rabbit example we will configure our docker environment, build the docker image and deploy it in our cluster.
+```
+minikube docker-env
+docker build -t kalaspuffar/kub-autoscaling-example .
+kubectl -n kube-system apply -f deploy.yml
+```
---
+++ autoscale.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+namespace=""
+deployment=""
+
+function getCurrentPods() {
+  # Retry up to 5 times if kubectl fails
+  for i in $(seq 5); do
+    current=$(kubectl -n $namespace describe deploy $deployment | \
+      grep desired | awk '{print $2}' | head -n1)
+
+    if [[ $current != "" ]]; then
+      echo $current
+      return 0
+    fi
+
+    sleep 3
+  done
+
+  echo ""
+}
+
+function notifySlack() {
+  if [ -z "$SLACK_HOOK" ]; then
+    return 0
+  fi
+
+  curl -s --retry 3 --retry-delay 3 -X POST --data-urlencode 'payload={"text": "'"$1"'"}' $SLACK_HOOK > /dev/null
+}
+
+autoscalingNoWS=$(echo "$AUTOSCALING" | tr -d "[:space:]")
+IFS=';' read -ra autoscalingArr <<< "$autoscalingNoWS"
+
+while true; do
+  for autoscaler in "${autoscalingArr[@]}"; do
+    IFS='|' read minPods maxPods mesgPerPod namespace deployment queueName <<< "$autoscaler"
+
+    queueMessagesJson=$(curl -s -S --retry 3 --retry-delay 3 -u "$RABBIT_USER:$RABBIT_PASS" \
+      $RABBIT_HOST:15672/api/queues/%2f/$queueName)
+
+    if [[ $? -eq 0 ]]; then
+      queueMessages=$(echo $queueMessagesJson | jq '.messages')
+      requiredPods=$(echo "$queueMessages/$mesgPerPod" | bc 2> /dev/null)
+
+      if [[ $requiredPods != "" ]]; then
+        currentPods=$(getCurrentPods)
+
+        echo "$(date) -- Current $namespace: $deployment has $currentPods pods ($queueMessages msg in RabbitMQ)"
+
+        if [[ $currentPods != "" ]]; then
+          if [[ $requiredPods -ne $currentPods ]]; then
+            desiredPods=""
+            # Flag used to prevent scaling down or up if currentPods are already min or max respectively.
+            scale=0
+
+            if [[ $requiredPods -le $minPods ]]; then
+              desiredPods=$minPods
+
+              # If currentPods are already at min, do not scale down
+              if [[ $currentPods -eq $minPods ]]; then
+                scale=1
+              fi
+            elif [[ $requiredPods -ge $maxPods ]]; then
+              desiredPods=$maxPods
+
+              # If currentPods are already at max, do not scale up
+              if [[ $currentPods -eq $maxPods ]]; then
+                scale=1
+              fi
+            else
+              desiredPods=$requiredPods
+            fi
+
+            if [[ $scale -eq 0 ]]; then
+              # To slow down the scale-down policy, scale down in steps (reduce 10% on every iteration)
+              if [[ $desiredPods -lt $currentPods ]]; then
+                desiredPods=$(awk "BEGIN { print int( ($currentPods - $desiredPods) * 0.9 + $desiredPods ) }")
+              fi
+
+              kubectl scale -n $namespace --replicas=$desiredPods deployment/$deployment 1> /dev/null
+
+              if [[ $? -eq 0 ]]; then
+                # Adjust logging and Slack notifications based on LOGS env and desiredPods number
+                log=false
+                avgPods=$(awk "BEGIN { print int( ($minPods + $maxPods) / 2 ) }")
+
+                if [[ $LOGS == "HIGH" ]]; then
+                  log=true
+                elif [[ $LOGS == "MEDIUM" && ($desiredPods -eq $minPods || $desiredPods -eq $avgPods || $desiredPods -eq $maxPods) ]]; then
+                  log=true
+                elif [[ $LOGS == "LOW" && ($desiredPods -eq $minPods || $desiredPods -eq $maxPods) ]]; then
+                  log=true
+                fi
+
+                if $log ; then
+                  echo "$(date) -- Scaled $namespace: $deployment to $desiredPods pods ($queueMessages msg in RabbitMQ)"
+                  notifySlack "Scaled $namespace: $deployment to $desiredPods pods ($queueMessages msg in RabbitMQ)"
+                fi
+              else
+                echo "$(date) -- Failed to scale $namespace: $deployment pods."
+                notifySlack "Failed to scale $namespace: $deployment pods."
+              fi
+            fi
+          fi
+        else
+          echo "$(date) -- Failed to get current pods number for $namespace: $deployment."
+          notifySlack "Failed to get current pods number for $namespace: $deployment."
+        fi
+      else
+        echo "$(date) -- Failed to calculate required pods for $namespace: $deployment."
+        notifySlack "Failed to calculate required pods for $namespace: $deployment."
+      fi
+    else
+      echo "$(date) -- Failed to get queue messages from $RABBIT_HOST for $namespace: $deployment."
+      notifySlack "Failed to get queue messages from $RABBIT_HOST for $namespace: $deployment."
+    fi
+  done
+
+  sleep $INTERVAL
+done
